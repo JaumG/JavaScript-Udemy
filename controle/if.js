bc9082d1f4128502cbcc334f9eed8ea152374d6e
JaumG <jguilhermepasco@gmail.com>
2023-01-31 15:33:04 -0300

    ifElse

--- controle/if.js
+++ controle/if.js
@@ -4,8 +4,8 @@ function soBoaNoticia(nota){
     }
 }
 
-soBoaNoticia(8.1)
-soBoaNoticia(6.1)
+// soBoaNoticia(8.1)
+// soBoaNoticia(6.1)
 
 function seForVerdadeiro(valor){
     if(valor){
@@ -14,14 +14,34 @@ function seForVerdadeiro(valor){
 }
 
 
-seForVerdadeiro()
-seForVerdadeiro(null)
-seForVerdadeiro(undefined)
-seForVerdadeiro(NaN)
-seForVerdadeiro('')
-seForVerdadeiro(0)
-seForVerdadeiro(-1)
-seForVerdadeiro(' ')
-seForVerdadeiro([])
-seForVerdadeiro([1, 2])
-seForVerdadeiro({})
+// seForVerdadeiro()
+// seForVerdadeiro(null)
+// seForVerdadeiro(undefined)
+// seForVerdadeiro(NaN)
+// seForVerdadeiro('')
+// seForVerdadeiro(0)
+// seForVerdadeiro(-1)
+// seForVerdadeiro(' ')
+// seForVerdadeiro([])
+// seForVerdadeiro([1, 2])
+// seForVerdadeiro({})
+
+function teste1(num){
+    if(num>7)
+        console.log(num)
+        console.log('Final')
+}
+
+teste1(6)
+teste1(8)
+
+function teste2(num){
+    if(num > 7);{ //não usar ponto e virgula
+        console.log(num)
+    }
+}
+
+teste2(6)
+teste2(8)
+                                             
+
